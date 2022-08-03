c67be90c226fe2f78d0c2619dbc1e475439f2d3d
ChanhyukPark-Tech <pgg6713@gmail.com>
2022-08-03 21:37:25 +0900

    feat: [instagram] 활동 페이지 퍼블리싱 완료

--- flutter_instagram_clone/lib/src/pages/active_history.dart
+++ flutter_instagram_clone/lib/src/pages/active_history.dart
@@ -1,12 +1,65 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/src/foundation/key.dart';
 import 'package:flutter/src/widgets/framework.dart';
+import 'package:flutter_instagram_clone/src/components/avatar_widget.dart';
 
 class ActiveHistory extends StatelessWidget {
   const ActiveHistory({Key? key}) : super(key: key);
 
+  Widget _activeItem() {
+    return Padding(
+        padding: const EdgeInsets.symmetric(vertical: 10),
+        child: Row(
+          children: const [
+            AvatarWidget(
+                type: AvatarType.TYPE2,
+                size: 40,
+                thumbPath: "https://picsum.photos/50"),
+            SizedBox(
+              width: 10,
+            ),
+            Expanded(
+              child: Text.rich(
+                TextSpan(
+                    text: "박찬혁",
+                    style: TextStyle(fontWeight: FontWeight.bold),
+                    children: [
+                      TextSpan(
+                          text: "님이 회원님의 게시물을 좋아합니다.",
+                          style: TextStyle(fontWeight: FontWeight.normal)),
+                      TextSpan(
+                          text: " 5d",
+                          style: TextStyle(
+                              fontWeight: FontWeight.normal,
+                              fontSize: 13,
+                              color: Colors.black54)),
+                    ]),
+              ),
+            )
+          ],
+        ));
+  }
+
   Widget _newRecentyleActiveView(String title) {
-    return Padding(padding: const EdgeInsets.symmetric());
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 15.0, vertical: 10),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: [
+          Text(
+            title,
+            style: const TextStyle(fontSize: 16),
+          ),
+          SizedBox(
+            height: 15,
+          ),
+          _activeItem(),
+          _activeItem(),
+          _activeItem(),
+          _activeItem(),
+        ],
+      ),
+    );
   }
 
   @override
