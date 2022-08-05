fcf046931f73c694dd2733b80eff96a67857fe80
ChanhyukPark-Tech <pgg6713@gmail.com>
2022-08-05 23:55:05 +0900

    feat: [flutter_ticket_book_app] finish global style init

---
+++ flutter_ticket_book_app/lib/screens/home.dart
@@ -0,0 +1,51 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/src/foundation/key.dart';
+import 'package:flutter/src/widgets/framework.dart';
+import 'package:flutter_ticket_book_app/utils/app_styles.dart';
+
+class HomeScreen extends StatelessWidget {
+  const HomeScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Styles.bgColor,
+      body: ListView(
+        children: [
+          Container(
+            padding: const EdgeInsets.symmetric(horizontal: 20),
+            child: Column(children: [
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(
+                        "Good morning",
+                        style: Styles.headLineStyle3,
+                      ),
+                      Text(
+                        "Book Tickets",
+                        style: Styles.headLineStyle1,
+                      ),
+                    ],
+                  ),
+                  Container(
+                    width: 50,
+                    height: 50,
+                    decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(10),
+                        image: const DecorationImage(
+                            fit: BoxFit.cover,
+                            image: AssetImage("assets/images/img_1.png"))),
+                  )
+                ],
+              )
+            ]),
+          )
+        ],
+      ),
+    );
+  }
+}
