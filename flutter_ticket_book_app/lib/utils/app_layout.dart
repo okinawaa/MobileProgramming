a0128750dbc6f79a6320c05af5ba5f634c8befd0
ChanhyukPark-Tech <pgg6713@gmail.com>
2022-08-07 02:16:16 +0900

    feat: [flutter_ticket_book_app] home screen 완료

--- flutter_ticket_book_app/lib/utils/app_layout.dart
+++ flutter_ticket_book_app/lib/utils/app_layout.dart
@@ -1,7 +1,28 @@
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
 
 class AppLayout {
   static getSize(BuildContext context) {
     return MediaQuery.of(context).size;
+  }
+
+  static getScreenWidth() {
+    return Get.width;
+  }
+
+  static getScreenHeight() {
+    return Get.height;
+  }
+
+  static getHeight(double pixels) {
+    double x = getScreenHeight() /
+        pixels; // height:800 , pixels : 200 => return 200  height : 1600 , pixels : 200 return 200
+    return getScreenHeight() / x;
+  }
+
+  static getWidth(double pixels) {
+    double x = getScreenWidth() /
+        pixels; // height:800 , pixels : 200 => return 200  height : 1600 , pixels : 200 return 200
+    return getScreenWidth() / x;
   }
 }
