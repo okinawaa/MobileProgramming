33e8fbe6b877c740c3575b7f9d0a47dc5e471efc
ChanhyukPark-Tech <pgg6713@gmail.com>
2022-09-22 23:10:27 +0900

    feat: chanhyuk_talk Stream util

---
+++ flutter_chanhyuk_talk/lib/screens/chat/chat_screen.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/src/foundation/key.dart';
+import 'package:flutter/src/widgets/framework.dart';
+
+class ChatScreen extends StatelessWidget {
+  const ChatScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {}
+}
--- flutter_chanhyuk_talk/lib/utils/stream_utils.dart
+++ flutter_chanhyuk_talk/lib/utils/stream_utils.dart
@@ -1,10 +1,29 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:flutter_chanhyuk_talk/constants/variables.dart';
 
 class StreamUtils {
   static final FirebaseFirestore _firestore = FirebaseFirestore.instance;
 
   Stream<QuerySnapshot<Map<String, dynamic>>> getUserStream() {
     // 빨대 꽃기
-    return _firestore.collection("user").snapshots();
+    return _firestore.collection(userCollection).snapshots();
+  }
+
+  // 내가 속해있는 채팅방 목록을 지켜봐라
+  Stream<QuerySnapshot<Map<String, dynamic>>> getChatRoomStream(String userId) {
+    return _firestore
+        .collection(chatRoomCollection)
+        .where('userIdList', arrayContains: userId)
+        .snapshots();
+  }
+
+  // 받은 채팅방에 채팅을 지켜봐라
+  Stream<QuerySnapshot<Map<String, dynamic>>> getChatStream(String chatRoomId) {
+    return _firestore
+        .collection(chatRoomCollection)
+        .doc(chatRoomId)
+        .collection(chatCollection)
+        .orderBy('timeStamp')
+        .snapshots();
   }
 }
