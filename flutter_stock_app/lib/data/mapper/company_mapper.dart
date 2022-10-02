0f33b682d16cfb5b1632aef63a3b343fe7ad22c0
ChanhyukPark-Tech <pgg6713@gmail.com>
2022-10-02 13:25:04 +0900

    [stock-app] comment for extension on

--- flutter_stock_app/lib/data/mapper/company_mapper.dart
+++ flutter_stock_app/lib/data/mapper/company_mapper.dart
@@ -3,7 +3,9 @@ import 'package:flutter_stock_app/data/source/remote/dto/company_info_dto.dart';
 import 'package:flutter_stock_app/domain/model/company_info.dart';
 import 'package:flutter_stock_app/domain/model/company_listing.dart';
 
-// Extension method는 특정 타입에 대해 메소드를 만들어주기 때문에, 해당 타입에서 자주 호출하게 될 메소드가 있을 때 이를 Extension method로 만들면 좋다. 예를 들어 List<int> 타입에 대해서 리스트 전체의 합을 구하는 경우가 많다고 생각해보자
+/* Extension method는 특정 타입에 대해 메소드를 만들어주기 때문에,
+ 해당 타입에서 자주 호출하게 될 메소드가 있을 때 이를 Extension method로 만들면 좋다.
+  예를 들어 List<int> 타입에 대해서 리스트 전체의 합을 구하는 경우가 많다고 생각해보자*/
 
 extension ToCompanyListing on CompanyListingEntity {
   CompanyListing toCompanyListing() {
